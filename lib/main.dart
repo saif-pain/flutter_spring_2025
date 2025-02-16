4d1ceeac95a6dfac5779ab39b769627eba150515
saif-pain <saifalmamun555@gmail.com>
2025-02-16 10:28:12 +0600

    task_2

--- lib/main.dart
+++ lib/main.dart
@@ -12,36 +12,20 @@ class AppHome extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("First App Flutter"),
+          title: Text("First App Flutter"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "4.0",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 5),
-              Row(
-                children: const [
-                  Icon(size: 40, Icons.star),
-                  Icon(size: 40, Icons.star),
-                  Icon(size: 40, Icons.star),
-                  Icon(size: 40, Icons.star),
-                  Icon(size: 40, Icons.star_border),
-                ],
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  print("Submit button pressed");
-                },
-                child: const Text("Submit"),
-              ),
-            ],
-          ),
+        body: Container(
+          height: double.infinity,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(size: 40, Icons.star),
+                Icon(size: 40, Icons.star),
+                Icon(size: 40, Icons.star),
+                Icon(size: 40, Icons.star),
+                Icon(size: 40, Icons.star_border)
+              ]),
         ),
       ),
     );
